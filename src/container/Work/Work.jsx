8a440bf73fffbcebb0b6559381cf944863714bf4
Ntami Egbe <ntamiegbe00@gmail.com>
2022-05-27 07:40:28 +0100

    Initial Commit

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { AiFillEye, AiFillGithub } from 'react-icons/ai'
+import { motion } from 'framer-motion'
 
+import { AppWrap } from '../../wrapper'
+import { urlFor, client } from '../../client'
 import './Work.scss';
+
 
 const Work = () => {
   return (
